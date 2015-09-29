0eca2f1edeac6e9089fd6df692d981678a4ec677
Nathan Berton <nathan.spero.berton@gmail.com>
2015-09-29 13:07:08 -0400

    Completed Step 1 of Lab 5

--- Lab5.md
+++ Lab5.md
@@ -1,6 +1,9 @@
 #Lab5
 
-1\.
+1\.Step 1 was all set up of the files and reading on what they individually 
+do. 
+
+Files Created in this step: cmakelists.txt, tutorial.cxx ,TuturialConfig.h
 
 2\.
 
--- lab5/tutorial.cxx
+++ lab5/tutorial.cxx
@@ -2,15 +2,21 @@
   COPY PASTED CODE FROM https://cmake.org/cmake-tutorial/
   For tutorial usage
 */
-
+  
 // A simple program that computes the square root of a number
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include "TutorialConfig.h"
+ 
 int main (int argc, char *argv[])
 {
   if (argc < 2)
     {
+    fprintf(stdout,"%s Version %d.%d\n",
+            argv[0],
+            Tutorial_VERSION_MAJOR,
+            Tutorial_VERSION_MINOR);
     fprintf(stdout,"Usage: %s number\n",argv[0]);
     return 1;
     }
@@ -20,3 +26,4 @@ int main (int argc, char *argv[])
           inputValue, outputValue);
   return 0;
 }
+

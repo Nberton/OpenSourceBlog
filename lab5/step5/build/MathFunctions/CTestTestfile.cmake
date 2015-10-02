cdec348dae22cab22e481bc200f10d7d6a40c0a7
Nathan Berton <nathan.spero.berton@gmail.com>
2015-10-02 13:29:14 -0400

    Step 5 Complete

---
+++ lab5/step5/build/MathFunctions/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/Documents/OpenSourceBlog/lab5/step5/MathFunctions
+# Build directory: /home/ubuntu/Documents/OpenSourceBlog/lab5/step5/build/MathFunctions
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
